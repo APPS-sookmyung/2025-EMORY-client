711af88855dbbe4f41357b1d15fb7d77fc4d06ff
김영교 <ygkgy0105@gmail.com>
2025-07-06 16:23:42 +0900

    Create App.tsx

---
+++ emoryclient/src/features/auth/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App: React.FC = () => {   //{: React.FC는 없어도 잘 동작합니다. }
+  return (
+    <div className="App">
+      <h1>MAIN</h1>
+    </div>
+  );
+}
+
+export default App;
